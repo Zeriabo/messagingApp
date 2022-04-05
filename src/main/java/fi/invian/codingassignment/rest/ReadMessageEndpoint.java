1b4473d0fc312aafaeb8cfe3db488fe7fba7283e
Zeriab Chiah <zeriab@hotmail.com>
2022-04-05 22:12:09 +0300

    decryption

--- src/main/java/fi/invian/codingassignment/rest/ReadMessageEndpoint.java
+++ src/main/java/fi/invian/codingassignment/rest/ReadMessageEndpoint.java
@@ -1,8 +1,12 @@
 package fi.invian.codingassignment.rest;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,9 +22,14 @@ import fi.invian.codingassignment.pojos.MessagesPojo;
 import fi.invian.codingassignment.pojos.ReceivePojo;
 
 import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidParameterSpecException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -45,11 +54,8 @@ public class ReadMessageEndpoint {
 		Response response = new Response();
 		MessagesPojo messages=new MessagesPojo();
 		
-		SecretKey secretKey=Encryption.generateKey(128);
-		Cipher ecipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		ecipher.init(Cipher.ENCRYPT_MODE, secretKey);
-		AlgorithmParameters params = ecipher.getParameters();
-	 byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+	
+
 		try (Connection c = DatabaseConnection.getConnection()) {
 
 			PreparedStatement p1 = c.prepareStatement("SELECT * FROM users where email=?");
@@ -63,7 +69,7 @@ public class ReadMessageEndpoint {
 				r.first();
 				if (userId > -1) {
 
-					PreparedStatement statement2 = c.prepareStatement("SELECT messages.messagebody,messages.title,messages.datetime,messages.idsender "
+					PreparedStatement statement2 = c.prepareStatement("SELECT messages.idmessages,messages.messagebody,messages.title,messages.datetime,messages.idsender "
 							+ "  FROM receiver, messages , users sender, users receivers "
 							+ "where receiver.messages_idmessages =  messages.idmessages\n"
 							+ "And sender.idusers=  messages.idsender\n"
@@ -75,12 +81,12 @@ public class ReadMessageEndpoint {
 					ResultSet rs = statement2.executeQuery();
 
 					while (rs.next()) {
-          String ms=Encryption.decrypt(ecipher.getAlgorithm(),rs.getString(1), secretKey, new IvParameterSpec(iv));
+          //String ms=Encryption.decrypt(ecipher.getAlgorithm(),rs.getString(1), secretKey, new IvParameterSpec(iv));
 						Message m = new Message();
-						m.setMessagebody(ms);
-						m.setDatetime((rs.getDate(3)));
-						m.setIdUser(rs.getInt(4));
-						m.setTitle(rs.getString(2));
+						m.setMessagebody(getMessageDecrypted(rs.getInt(1),rs.getString(2),c));
+						m.setDatetime((rs.getDate(4)));
+						m.setIdUser(rs.getInt(5));
+						m.setTitle(rs.getString(3));
 						System.out.println(m.toString());
 						messages.addMessage(m);
 					messages.getMessages().forEach((mess)->{System.out.print(mess);});
@@ -110,5 +116,28 @@ public class ReadMessageEndpoint {
 		} 
 		return response;
 
+	}
+
+	private String getMessageDecrypted(int i,String message, Connection c) throws SQLException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
+		
+		String ms=null;
+		PreparedStatement pGetkey = c.prepareStatement("SELECT * FROM messaging.secret_keys where messages_idmessages=?");
+		pGetkey.setInt(1, i);
+		
+		ResultSet rs = pGetkey.executeQuery();
+		while(rs.next()) {
+			byte[] secretKeyBytes=rs.getBytes(2);
+			SecretKey key = new SecretKeySpec(secretKeyBytes, 0, secretKeyBytes.length, "AES");
+			Cipher dcipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			
+			AlgorithmParameters params = dcipher.getParameters();
+		 byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+		  dcipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+		 ms=Encryption.decrypt("AES/CBC/PKCS5Padding",message, key, new IvParameterSpec(iv));
+			
+			
+		}
+		
+		return ms;
 	}
 }
