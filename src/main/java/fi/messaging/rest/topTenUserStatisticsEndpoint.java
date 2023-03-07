afa7635af39d669ad791ae7be301457e99f293cf
Zeriab Chiah <zeriab@hotmail.com>
2023-03-07 14:12:37 +0200

    statisticsService

--- src/main/java/fi/messaging/rest/topTenUserStatisticsEndpoint.java
+++ src/main/java/fi/messaging/rest/topTenUserStatisticsEndpoint.java
@@ -11,14 +11,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
+import java.util.ServiceLoader;
+
 import fi.messaging.app.DatabaseConnection;
 import fi.messaging.service.StatisticsService;
+import fi.messaging.service.TokenService;
 
 import javax.ws.rs.core.Response;
 @Path("/readuserstatistics")
 
 public class topTenUserStatisticsEndpoint {
 	int row = 0;
+	
+	StatisticsService statisticsService=getStatisticsService();
 	
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -27,6 +32,15 @@ public class topTenUserStatisticsEndpoint {
 	public Response readStatistics() throws Exception {
 
 		
-		return StatisticsService.getStatistics();
+		return statisticsService.getStatistics();
 }
+	
+	public static StatisticsService getStatisticsService() {
+	    // load our plugin
+	 ServiceLoader<StatisticsService> serviceLoader =ServiceLoader.load(StatisticsService.class);
+	 for (StatisticsService provider : serviceLoader) {
+	     return provider;
+	 }
+	 throw new NoClassDefFoundError("Unable to load a driver "+StatisticsService.class.getName());
+	}
 }
